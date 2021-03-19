7afdc7132eaa66641f118a35fe26087dc144ecf0
Beau Jennings <redalthazar@gmail.com>
2021-03-19 14:46:17 -0500

    fixed errors in Todo and User. New error when running build. o.s.boot.SpringApplication

--- src/main/java/com/lambdaschool/todos/models/Todos.java
+++ src/main/java/com/lambdaschool/todos/models/Todos.java
@@ -27,6 +27,7 @@ public class Todos extends Auditable{
     public Todos(User user, String description) {
         this.user = user;
         this.description = description;
+        this.completed = false;
     }
 
     public long getTodoid() {
